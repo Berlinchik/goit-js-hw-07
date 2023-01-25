9278e4ecce947c71dc519c132f6ba0a074309191
Berlinchik <davidarts11@gmail.com>
2023-01-25 22:20:24 +0200

    update js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const mainDiv = document.querySelector(".gallery");
+function createGalaryMarkup(array) {
+  return array
+    .map(
+      ({ original, preview, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
 
-console.log(galleryItems);
+mainDiv.insertAdjacentHTML("beforeend", createGalaryMarkup(galleryItems));
+
+mainDiv.addEventListener("click", onImgClick);
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+        <img
+      src="${e.target.dataset.source}"
+    />
+`);
+  instance.show();
+
+  mainDiv.addEventListener("keydown", closeImg);
+  function closeImg(e) {
+    if (e.code === "Escape") {
+      instance.close();
+      mainDiv.removeEventListener("keydown", closeImg);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galaryList = document.querySelector(".gallery");
+function createGalaryMarkup(array) {
+  return array
+    .map(
+      ({
+        original,
+        preview,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join("");
+}
+galaryList.insertAdjacentHTML("beforeend", createGalaryMarkup(galleryItems));
+
+galaryList.addEventListener("click", onImgClick);
+
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "A") {
+    return;
+  }
+}
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
